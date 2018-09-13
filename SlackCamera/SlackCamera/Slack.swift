22bdeebb6b91f7ece4e65c1c986e6f475f151e8c
Yuki Yasoshima <yukiyasos@gmail.com>
2018-09-13 19:21:41 +0900

    エラー処理

--- SlackCamera/SlackCamera/Slack.swift
+++ SlackCamera/SlackCamera/Slack.swift
@@ -7,35 +7,53 @@ import Foundation
 import UIKit
 
 class Slack {
+    enum PostError: Error {
+        case resizeImage
+        case jpegRepresentation
+        case loadPlistUrl
+        case loadPlist
+        case loadChannelID
+        case loadToken
+        case createUrlComponents
+        case getUrl
+    }
+    
     init() {
     }
     
     class func post(image: UIImage, filename: String, completion: @escaping (Error?) -> Void) {
         guard let resizedImage = resize(image: image) else {
+            self.call_async(error: .resizeImage, completion: completion)
             return
         }
         
         guard let jpegData = UIImageJPEGRepresentation(resizedImage, 1.0) else {
+            self.call_async(error: .jpegRepresentation, completion: completion)
             return
         }
         
         guard let plistUrl = Bundle.main.url(forResource: "slack", withExtension: "plist") else {
+            self.call_async(error: .loadPlistUrl, completion: completion)
             return
         }
         
         guard let plist = NSDictionary(contentsOf: plistUrl) else {
+            self.call_async(error: .loadPlist, completion: completion)
             return
         }
         
         guard let channelID = plist["channel_id"] as? String else {
+            self.call_async(error: .loadChannelID, completion: completion)
             return
         }
         
         guard let token = plist["bot_token"] as? String else {
+            self.call_async(error: .loadToken, completion: completion)
             return
         }
         
         guard var components = URLComponents(string: "https://slack.com/api/files.upload") else {
+            self.call_async(error: .createUrlComponents, completion: completion)
             return
         }
         
@@ -43,6 +61,7 @@ class Slack {
                                  URLQueryItem(name: "channels", value: channelID)]
         
         guard let url = components.url else {
+            self.call_async(error: .getUrl, completion: completion)
             return
         }
         
@@ -92,5 +111,11 @@ extension Slack {
         UIGraphicsEndImageContext()
         
         return resizedImage
+    }
+    
+    class private func call_async(error: PostError, completion: @escaping (Error?) -> Void) {
+        DispatchQueue.main.async {
+            completion(error)
+        }
     }
 }
